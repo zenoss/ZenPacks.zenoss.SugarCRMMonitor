09e7991a0d14f6cd6ef2ddcc93d117640b4d21df
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2013-06-26 16:10:47 +0000

    Remove Zenoss from INSTALL_REQUIRES.
    This is to allow trunk ZenPacks to be tested on current stable Zenoss
    versions.
    
    Reviewed by gstreiff and jcausey.
    
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.SugarCRMMonitor@74544 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ VERSION = '2.2.1'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
-INSTALL_REQUIRES = ['ZenPacks.zenoss.ZenWebTx', 'Zenoss >= 2.2']
+INSTALL_REQUIRES = ['ZenPacks.zenoss.ZenWebTx']
 COMPAT_ZENOSS_VERS = '>= 2.2'
 PREV_ZENPACK_NAME = 'SugarCRMMonitor'
 # STOP_REPLACEMENTS
